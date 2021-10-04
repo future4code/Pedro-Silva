b006b196e369de9c2c5ea17bae25a02cfdb1bc8f
pedrohenriqueduarte <ph09duarte@gmail.com>
2021-10-04 16:34:30 -0300

    exercicios-variaveis-feito

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,68 @@
+//Exercícios de interpretação de código
+
+/* Exercício 1
+Será impresso:
+10
+10 5
+
+Exercício 2
+Será impresso: 
+10 10 10
+
+Exercício 3
+Melhores nomes para as variáveis:
+p - horasTrabalhadas
+t - pagamentoDiario
+
+*/ 
+
+// Exercícios de escrita de código
+
+// Exercício 1 
+// Programa exercício 1:
+/*
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual a sua idade?")
+
+console.log("Olá,", nome, "você tem", idade ,"anos.")
+
+console.log(typeof nome, typeof idade) 
+
+
+
+// Resposta d: Foi impresso string e number, respectivamente. Pois um se configura enquanto texto(conjunto de caracteres) e outro enquanto número.
+// Resposta f: Notei que o tipo number passou a ser string. 
+
+// Exercício 2
+// Programa exercício 2:
+
+const musica = "Sim."
+const acordouCedo = "Sim."
+const cansado = "Não."
+
+console.log("Você está ouvindo música?", musica, "Você acordou cedo hoje?", acordouCedo)
+console.log("Você está cansado?", cansado)
+
+
+//Exercício 3
+//Programa exercício 3
+let a = 10
+let b = 25
+
+c = a 
+a = b
+b = c 
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+*/
+
+//Desafio
+/*
+const z = Number(prompt("Me diga um número"))
+const s = Number (prompt("Diga mais um número"))
+const x = (z + s)
+const y = (z * s)
+console.log("O primeiro número somado ao segundo número resulta em:", x, ".")
+console.log("O primeiro número multiplicado pelo segundo número resulta em:", y, ".")
+*/
