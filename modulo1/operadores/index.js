e96989821d945a128052e69687ae826c9d92aba4
pedrohenriqueduarte <ph09duarte@gmail.com>
2021-10-05 15:17:37 -0300

    exercicios de operaçoes feitos

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,92 @@
+// Exercício aula Operadores
+
+// Exercícios de interpretação de código
+/* 
+Exercício 1
+
+ a. false
+ b. false
+ c. true
+ d. boolean
+
+Exercício 2 
+
+Sim, consigo perceber um problema. A variável está em string.
+Será impresso no console os números como strings, um ao lado do outro. 
+Por exemplo: primeiroNumero = 3 segundoNumero - 4
+Será impresso 3 e 4 um ao lado do outro. (34)
+
+Exercício 3
+
+Adicionar Number antes de cada variável na declaração da variável soma.
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = Number(primeiroNumero) + Number(segundoNumero)
+
+console.log(soma)
+
+*/ 
+
+// Exercícios de escrita de código
+
+//Exercício 1
+
+// const idadeUsuario = prompt("Sua idade:") // 23
+// const idadeAmiga = prompt("Idade da sua melhor amiga:") // 22
+
+// console.log("Sua idade é maior do que a da sua melhor amiga?", Number(idadeUsuario) > Number(idadeAmiga)) //Resposta: true
+
+// const diferençaIdade = Number(idadeUsuario) - Number(idadeAmiga)
+
+// console.log(diferençaIdade) // Resposta: 1
+
+// Exercício 2 
+
+// const numeroPar = prompt("Insira um número par:") // 6
+
+// const restoDaDivisao = Number(numeroPar) % 2
+
+// console.log(restoDaDivisao)
+
+// Resposta c: Há um padrão. Toda operação dá o resto da divisão como 0,
+// por ser uma divisão por 2 de números pares. 
+
+// Resposta d: Se inserir um número ímpar haverá um valor diferente de 0
+// como resto da divisão. No caso, 1. 
+
+// Exercício 3
+
+// const idadeUsuario = prompt("Qual a sua idade?")
+
+// const idadeMeses = Number(idadeUsuario) * 12
+// const idadeDias = Number(idadeUsuario) * 365
+// const idadeHoras = Number(idadeUsuario) * (365 *24)
+
+// console.log(idadeMeses, idadeDias, idadeHoras)
+
+// Exercício 4
+
+// const x = Number(prompt("Diga um número:")) // 9
+// const y = Number(prompt("Diga mais um número:")) // 3
+
+// const primeiroResultado = x > y
+// const segundoResultado = x === y
+// const terceiroResultado = (x % y) === 0
+// const quartoResultado = (y % x) === 0 
+
+// console.log("O primeiro numero é maior que o segundo?", primeiroResultado)
+// console.log("O segundo numero é igual ao segundo?", segundoResultado)
+// console.log("O primeiro numero é divisível pelo segundo?", terceiroResultado)
+// console.log("O segundo numero é divisível pelo primeiro?", quartoResultado)
+
+
+
+
+
+
+
+
+
+
