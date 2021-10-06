8e8cbb72d86ab2202c03a6c93a87a97f3301381a
pedrohenriqueduarte <ph09duarte@gmail.com>
2021-10-06 16:37:24 -0300

    exercicios strings e arrays feito

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,85 @@
+// Exercícios Aula Strings e Arrays
+
+/* 
+Exercícios de interpretação de código
+
+Exercício número 1
+
+a. undefined
+
+b. null
+
+c. 11
+
+d. 3
+
+e. [3, 19, 5, 6, 7, 8, 9 , 10, 11, 12, 13]
+
+f. 9
+
+Exercício número 2
+
+SUBA NUM ÔNIBUS EM MIRROCOS 27 
+
+*/
+
+//Exercícios de escrita de código
+
+// Exercício 1. 
+
+// const nomeDoUsuario = prompt("Digite seu nome:")
+// const emailDoUsuario = prompt("Digite agora seu e-mail:")
+
+// console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o) ${nomeDoUsuario}!`)
+
+// Exercício 2.
+
+// const comidas = ["batata frita", "picanha", "hamburguer", "lasanha", "bolo"]
+// console.log(comidas)
+
+// console.log(`Essas são as minhas comidas preferidas: 
+// ${comidas[0]}
+// ${comidas[1]}
+// ${comidas[2]}
+// ${comidas[3]}
+// ${comidas[4]}`)
+
+// const comidaUsuario = prompt("Olá Usuário, qual sua comida preferida?")
+// comidas.splice(1,1)
+// comidas.push(comidaUsuario)
+// console.log(comidas)
+
+// Exercício 3. 
+
+// const listaDeTarefas = []
+
+// const primeiraTarefa = prompt("Diga uma tarefa que precisa realizar:")
+// const segundaTarefa = prompt("Diga a segunda tarefa que precisa realizar:")
+// const terceiraTarefa = prompt("Diga a terceira tarefa que precisa realizar:")
+// listaDeTarefas.push(primeiraTarefa, segundaTarefa, terceiraTarefa)
+
+// console.log(listaDeTarefas)
+
+// const tarefaRealizada = prompt("Qual tarefa você já realizou? Diga entre os números 0, 1 ou 2, respectivamente.")
+// const tarefasConcluidas = listaDeTarefas.splice(tarefaRealizada,1)
+
+// console.log(listaDeTarefas.splice(tarefasConcluidas))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
