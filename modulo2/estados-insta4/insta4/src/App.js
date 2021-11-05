3f810dceabb377e43a2f809210c96c6d0de3f062
pedrohenriqueduarte <ph09duarte@gmail.com>
2021-11-05 17:54:15 -0300

    exercitando arrays e componentes

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,27 +10,89 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+
+    usuariosPosts : [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: 'usuário123',
+        fotoUsuario: 'https://picsum.photos/id/1060/536/354?blur=2',
+        fotoPost: 'https://picsum.photos/seed/picsum/536/354'
+      },
+      {
+        nomeUsuario: 'usuário321',
+        fotoUsuario: 'https://picsum.photos/536/354',
+        fotoPost: 'https://picsum.photos/id/870/536/354?grayscale&blur=2'
+      }
+    ],
+    valorInputNomeUsuario:'',
+    valorInputFotoUsuario: '',
+    valorInputFotoPost:''
+  }
+
+  incluirPost = () => {
+    const postNovo = {
+      nomeUsuario: this.state.valorInputNomeUsuario,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputFotoPost
+    }
+
+    const feedAtualizado = [...this.state.usuariosPosts, postNovo]
+
+    this.setState({usuariosPosts: feedAtualizado})
+    this.setState({valorInputNomeUsuario: ''})
+    this.setState({valorInputFotoUsuario: ''})
+    this.setState({valorInputFotoPost: ''})
+  }
+
+  onChangeInputNomeUsuario = (event) => {
+    this.setState({valorInputNomeUsuario: event.target.value})
+  }
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({valorInputFotoUsuario: event.target.value})
+  }
+
+  onChangeInputFotoPost = (event) => {
+    this.setState({valorInputFotoPost: event.target.value})
+  }
+
+
+
+
   render() {
+
+    const listaPost = this.state.usuariosPosts.map((item) => {
+      return <Post
+      nomeUsuario={item.nomeUsuario}
+      fotoUsuario={item.fotoUsuario}
+      fotoPost={item.fotoPost}
+      />
+    })
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-        <Post
-          nomeUsuario={'usuário123'}
-          fotoUsuario={'https://picsum.photos/id/1060/536/354?blur=2'}
-          fotoPost={'https://picsum.photos/seed/picsum/536/354'}
-        />
-        
-        <Post
-          nomeUsuario={'usuário321'}
-          fotoUsuario={'https://picsum.photos/536/354'}
-          fotoPost={'https://picsum.photos/id/870/536/354?grayscale&blur=2'}
-        />
-      </MainContainer>
+        <input
+      placeholder={"Nome de Usuário"}
+      value= {this.state.valorInputNomeUsuario}
+      onChange={this.onChangeInputNomeUsuario}
+      />
+      <input
+      placeholder={"Foto de Usuário"}
+      value= {this.state.valorInputFotoUsuario}
+      onChange={this.onChangeInputFotoUsuario}
+      />
+      <input
+      placeholder={"Foto do post"}
+      value={this.state.valorInputFotoPost}
+      onChange={this.onChangeInputFotoPost}
+      />
+      <button onClick={this.incluirPost}>Enviar</button>
+        {listaPost}
+        </MainContainer>
     );
   }
 }
