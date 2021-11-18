e837b27f25d5529125cda587d5c95bf46598c9e5
pedrohenriqueduarte <ph09duarte@gmail.com>
2021-11-18 18:56:47 -0300

    listagem playlist

---
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,110 @@
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const CardPlaylist = styled.div`
+
+`
+
+export default class App extends React.Component {
+  state = { 
+    playlistName: '',
+    playlists: []
+
+  }
+
+  componentDidMount() {
+    this.getAllPlaylists()
+  }
+
+  getAllPlaylists = () => {
+    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists', {
+      headers: {
+        Authorization: 'pedro-silva-carver'
+      }
+    })
+
+      .then((response) => {
+        this.setState({ playlists: response.data.result.list })
+        console.log(response)
+      })
+
+      .catch((erro) => {
+        alert("Erro, tente novamente.")
+      })
+  }
+
+  createPlaylist = () => {
+    const body = {
+      name: this.state.playlistName,
+    }
+    axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists', body, {
+      headers: {
+        Authorization: 'pedro-silva-carver'
+      }
+    })
+
+      .then(() => {
+        this.setState({ playlistName: '' })
+        this.getAllPlaylists()
+        alert('Playlist Criada!')
+      })
+
+      .catch(() => {
+        alert('Erro.')
+      })
+  }
+
+  deletePlaylist = (playlistId) => {
+    axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistId}`, {
+      headers: {
+        Authorization: 'pedro-silva-carver'
+      }
+    })
+      .then(() => {
+        alert('Playlist Deletada')
+        this.getAllPlaylists()
+      })
+
+      .catch ((error) => {
+        alert('Erro')
+        console.log(error.response)
+      })
+  }
+
+  onChangePlaylistName = (event) => {
+    this.setState({ playlistName: event.target.value })
+  }
+
+
+  render() {
+
+    const listaDePlaylists = this.state.playlists.map ((item) => {
+      return <div>
+        <li>
+          {item.name} 
+        <button onClick={() => this.deletePlaylist(item.id)}>Deletar Playlist</button>
+        </li>
+        </div>
+    })
+
+
+
+
+
+
+    return (
+      <div>
+        <h2>Playlists</h2>
+        <input
+        placeholder='Nome da Playlist'
+        onChange={this.onChangePlaylistName}
+        />
+        <button onClick={this.createPlaylist}>Criar Playlists</button>
+        <div>
+          {listaDePlaylists}
+        </div>
+      </div>
+    )
+  }
+}
