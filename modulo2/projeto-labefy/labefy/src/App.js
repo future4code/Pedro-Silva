891a608a8feab9e4332d0650a85064934bd15548
pedrohenriqueduarte <ph09duarte@gmail.com>
2021-11-19 17:30:18 -0300

    funcionalidades feitas falta estilizar

--- modulo2/projeto-labefy/labefy/src/App.js
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -24,8 +24,11 @@ export default class App extends React.Component {
   state = {
     playlistName: '',
     playlists: [],
-    playlistTracks:[]
-
+    playlistTracks: [],
+    musicName: '',
+    artistaName: '',
+    urlLink: '',
+    playlistId: ''
   }
 
   componentDidMount() {
@@ -95,7 +98,7 @@ export default class App extends React.Component {
     })
 
       .then((response) => {
-        this.setState({ playlists: response.data.result.tracks })
+        this.setState({ playlistTracks: response.data.result.tracks, playlistId: id })
       })
 
       .catch((erro) => {
@@ -103,7 +106,34 @@ export default class App extends React.Component {
       })
   }
 
-  
+  addTrackToPlaylist = () => {
+    const body = {
+      name: this.state.musicName,
+      artist: this.state.artistaName,
+      url: this.state.urlLink
+    }
+    axios.post(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.state.playlistId}/tracks`, body, {
+      headers: {
+        Authorization: 'pedro-silva-carver'
+      }
+    })
+
+      .then((response) => {
+        this.setState({
+          musicName: '',
+          artistaName: '',
+          urlLink: '',
+        })
+        alert('Música Adicionada!')
+      })
+
+      .catch((erro) => {
+        console.log(erro.response)
+        alert("Erro, tente novamente.")
+      })
+  }
+
+
 
 
 
@@ -111,52 +141,72 @@ export default class App extends React.Component {
     this.setState({ playlistName: event.target.value })
   }
 
+  onChangeMusicName = (event) => {
+    this.setState({ musicName: event.target.value })
+  }
+
+  onChangeArtistName = (event) => {
+    this.setState({ artistaName: event.target.value })
+  }
+
+  onChangeUrlLink = (event) => {
+    this.setState({ urlLink: event.target.value })
+  }
+
 
   render() {
 
     const listaDePlaylists = this.state.playlists.map((item) => {
-      return <CardPlaylist>
+      return <CardPlaylist key={item.id}>
 
-          <p>{item.name}</p>
-          <p>{item.artist}</p>
-        
+        <p>{item.name}</p>
+
         <button onClick={() => this.getPlaylistsTracks(item.id)}>Ver Playlist</button>
         <button onClick={() => this.deletePlaylist(item.id)}>Deletar</button>
       </CardPlaylist>
     })
 
     const trackList = this.state.playlistTracks.map((music) => {
-      return <div>
-        <li>{music.name}</li>
-        <li>{music.artist}</li>
+      return <div key={music.id}>
+        {/* <p> {music.name} - {music.artist} </p> */}
+        <iframe src={music.url} width='20%' height='80' frameBorder="0" allowfullscreen="" allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
       </div>
     })
-
-
-
-
-
 
     return (
       <div>
         <GlobalStyle />
         <div>
-          <h2> Bem-vinde ao Labefy! </h2>
+          <h2> Bem-vindo ao Labefy! </h2>
           <h3> Crie suas Playlists: </h3>
           <input
             placeholder='Nome da Playlist'
             onChange={this.onChangePlaylistName}
           />
           <button onClick={this.createPlaylist}>Criar Playlists</button>
-          <hr/>
+          <hr />
           <div>
             <h2>Suas Playlists:</h2>
             {listaDePlaylists}
+            <h2>Detalhes da Playlist:</h2>
+            {trackList}
           </div>
         </div>
-        <hr/>
+        <hr />
         <div>
           <h2> Adicionar Música à Playlist</h2>
+          <input
+            placeholder='Nome da Música'
+            onChange={this.onChangeMusicName}
+          />
+          <input placeholder='Artista'
+            onChange={this.onChangeArtistName}
+          />
+          <input placeholder='Link de aúdio'
+            onChange={this.onChangeUrlLink}
+          />
+          <button onClick={this.addTrackToPlaylist}>Adicionar na Playlist</button>
+
         </div>
       </div>
     )
