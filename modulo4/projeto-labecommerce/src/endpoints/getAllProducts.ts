c1eff4d9b2b7897b9da5de3d6ba8495a2737c1f7
pedrohenriqueduarte <ph09duarte@gmail.com>
2022-02-11 16:40:43 -0300

    desafio 1

--- modulo4/projeto-labecommerce/src/endpoints/getAllProducts.ts
+++ modulo4/projeto-labecommerce/src/endpoints/getAllProducts.ts
@@ -7,9 +7,42 @@ export const getAllProducts = async (
     res: Response
 ): Promise<any> => {
     try {
-        const result: Product = await connection('labecommerce_products')
-        res.status(200).send({ products: result })
+        let order = req.query.order
+        let sort = req.query.sort
+        let search = req.query.search
 
+        let result: Product[] = await connection('labecommerce_products')
+
+        if (!order && !sort) {
+            result = await connection('labecommerce_products')
+        } else if (!search) {
+            result = await connection('labecommerce_products')
+        }
+
+        if (order !== 'asc' && order !== 'desc') {
+            order = 'asc'
+        }
+
+        if (sort !== 'name' && sort !== 'id') {
+            sort = ''
+        }
+
+        if (search) {
+            result = await connection('labecommerce_products')
+                .where('name', 'LIKE', `%${search}%`)
+        }
+
+        if (order && sort) {
+            result = await connection('labecommerce_products')
+                .orderBy(sort, order)
+        } 
+
+        if(order && sort && search) {
+            result = await connection('labecommerce_products')
+                .where('name', 'LIKE', `%${search}%`)
+                .orderBy(sort, order)
+        }
+        res.status(200).send({ products: result })
     } catch (error: any) {
         res.status(500).send({ message: error.message || error.sqlMessage })
     }
